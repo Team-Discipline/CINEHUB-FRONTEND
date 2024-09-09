9aa11fd436539061e440d70f10a7a0d5f3b3f53a
Koul <dongik0122@gmail.com>
2024-09-09 16:16:09 +0900

    github action with aws ec2

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,8 +1,8 @@
 'use client'
 
 import React from 'react';
-import Input from "@/components/Input";
-import Button from "@/components/Button";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 import { motion } from "framer-motion"
 import Link from "next/link";
 
